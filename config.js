c6d8d2a7334850375bf100281a0d0115ec72095a
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-02-19 06:14:43 +0100

    Update config.js

--- config.js
+++ config.js
@@ -36,5 +36,5 @@ module.exports = {
     OWNER_NAME: process.env.OWNER_NAME || "𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑",
     OWNER_NUMBER: process.env.OWNER_NUMBER || "2348144250768",
     PREFIX: process.env.PREFIX || ".",
-    SESSION_ID: process.env.SESSION_ID || "HFZD1aQL#LsYqpqDnPfpWlTMx86Z875A_sz-dm2q8lBih5mNSNEA"
+    SESSION_ID: process.env.SESSION_ID || "qcgBnSLb#zHmKxAtWL-bPc5DJOGMjwFxNuQbNZP6sLEE87bNB14g"
 };
